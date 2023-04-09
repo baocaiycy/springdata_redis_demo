dd73efe8adeeb4989e4aa8aca5dc35feeb02e97a
ycy <bxyycy@163.com>
2023-04-09 10:48:16 +0800

    perf: 定义变量

--- src/main/java/com/ycy/demo/SpringdataRedisDemoApplication.java
+++ src/main/java/com/ycy/demo/SpringdataRedisDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdataRedisDemoApplication {
 
     public static void main(String[] args) {
+        int i = 0;
         SpringApplication.run(SpringdataRedisDemoApplication.class, args);
     }
 
